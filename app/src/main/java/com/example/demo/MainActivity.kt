d9de2bb75d3fe4cc629d024d94d511efc914165f
hecuncun <hecuncun@moqing.com>
2021-08-17 09:34:28 +0800

    222

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.demo
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 
 class MainActivity : AppCompatActivity() {
+
+    private val handler = @SuppressLint("HandlerLeak")
+    object :Handler(){
+        @SuppressLint("HandlerLeak")
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        val msg = Message.obtain()
+        msg.what = 0
+        msg.obj = "msg1"
+        handler.sendMessage(msg)
     }
 }
